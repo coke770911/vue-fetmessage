52e8bb9d6ba53de3f59ab64f95008676c8660579
fz083 <fz083@mail.aeust.edu.tw>
2023-11-01 15:42:19 +0800

    初始化專案

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import HelloWorld from './components/HelloWorld.vue'
+</script>
+
+<template>
+  
+  <header>
+    <div class="container">
+      <nav>
+        <RouterLink to="/">Home</RouterLink>
+        <RouterLink to="/about">About</RouterLink>
+      </nav>
+    </div>
+    <div class="wrapper">
+      <HelloWorld msg="You did it!" />
+    </div>
+  </header>
+
+  <RouterView /> 
+  <footer class="fixed-bottom">
+    <div class="container">
+      <p>© 2022 圖書資訊處建置 本系統建議使用Chrome Edge等瀏覽器。</p>
+    </div>
+  </footer>
+
+  
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+</script>
+<template>
+  <main>
+    <form id="loginform" action="api/login" method="post">
+      <h1 class="h3 mb-3 fw-normal">簡訊平台登入</h1>
+      <div class="form-floating">
+        <input class="form-control" type="text" name="account" id="account" placeholder="Portal 帳號">
+        <label for="account">Portal 帳號</label>
+      </div>
+      <div class="form-floating">
+        <input class="form-control" type="password" name="password" id="password" placeholder="Portal 密碼">
+        <label for="password">Portal 密碼</label>
+      </div>
+        <button class="w-100 btn btn-lg btn-primary" type="button">確認登入</button>
+    </form>
+  </main>
+</template>
